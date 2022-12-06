a8a9d40b260f1ad35c9b11d0f1734ee0871d66f3
Rasoul Veisi <rasoulweysi@gmail.com>
2022-12-06 08:02:03 +0330

    Meals folder updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import Header from "./compnents/Layout/Header";
+import Meals from "./compnents/Meal/Meals";
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/compnents/Meal/MealsAvailable.js
+++ src/compnents/Meal/MealsAvailable.js
@@ -1,7 +1,40 @@
-import classes from './MealsAvailable.modules.css'
+import classes from "./MealsAvailable.module.css";
 
-const MealsAvailable = () =>{
-    return()
-}
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
 
-export default MealsAvailable 
+const MealsAvailable = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+
+  return (
+    <section className={classes.meals}>
+      <ul>{mealsList}</ul>
+    </section>
+  );
+};
+
+export default MealsAvailable;
